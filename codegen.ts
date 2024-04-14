9d772cb3ba82156a1194ef2927b631073cddb808
Ashwin Menghar <ashwinmenghar123@gmail.com>
2024-04-14 12:43:50 +0530

    env added

--- codegen.ts
+++ codegen.ts
@@ -1,19 +1,19 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:8000/graphql",
+  schema: "https://d25kkaiq74g81m.cloudfront.net/graphql",
+  // schema: "http://localhost:8000/graphql",
   documents: "**/*.{tsx,ts}",
   generates: {
     "gql/": {
       preset: "client",
-      plugins: []
+      plugins: [],
     },
     "./graphql.schema.json": {
-      plugins: ["introspection"]
-    }
-  }
+      plugins: ["introspection"],
+    },
+  },
 };
 
 export default config;
